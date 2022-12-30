8e525672fd5f8ab09c9ffcc14976fc517c37ada7
Gustavo Marianelli <101139455+GustavoMarianelli@users.noreply.github.com>
2022-12-30 09:36:17 -0300

    enum

--- Curso Cpp YouTube/base.cpp
+++ Curso Cpp YouTube/base.cpp
@@ -2,43 +2,15 @@
 
 using namespace std;
 
-void texto();
-
-void soma(int n1, int n2);
-
-void soma(double n1, double n2);
-
-void mostrarNaTela(string txt = "Gustavo"){
-    cout << endl << txt << endl;
-}
-
-void contador(int num, int i = 0){
-    
-    if(i < num){
-        cout << i << " ";
-        contador(num, ++i);
-    }
-    cout << endl;
-}
-
 int main(int argc, char const *argv[]){
-    // texto();
-    // soma(2,3);
-    // soma(1.0, 2.5);
-    // mostrarNaTela();
-    contador(5);
+    enum armas {fuzil = 150, faca = 2, pistola = 6, escopeta = 12};
+
+    armas armaSelecionada;
+
+    armaSelecionada = fuzil;
+
+    cout << armaSelecionada << endl;
 
     return 0;
 }
 
-void texto(){
-    cout << "Texto\n";
-}
-
-void soma(int n1, int n2){
-    cout << n1 + n2 << endl;
-}
-
-void soma(double n1, double n2){
-    cout << n1 + n2 << endl;
-}
